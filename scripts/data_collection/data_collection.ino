50b7d940b7dabd55d557994d10840ebab9f5b1c2
Tuguldur Baterdene <136295598+SirTugulur@users.noreply.github.com>
2026-05-17 13:32:35 -0700

    Add files via upload

---
+++ scripts/data_collection/data_collection.ino
@@ -0,0 +1,34 @@
+#include <Arduino_BMI270_BMM150.h>
+
+// simple code to read 3 positional axes
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  delay(2000);
+  
+  if (!IMU.begin()) {
+    Serial.println("Failed to initialize IMU.");
+    while(1);
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  float ax, ay, az // acceleration
+  float gx, gy, gz // gyroscope
+  float mx, my, mz // magnetic field
+
+  if (IMU.accelerationAvailable()) {
+    IMU.readAcceleration(ax,ay,az)
+  }
+
+  if (IMU.gyroscopeAvailable()) {
+    IMU.readGyroscope(gx,gy,gz)
+  }
+
+  if (IMU.magneticFieldAvailable()) {
+    IMU.readMagneticField(mx,my,mz);
+  }
+
+  delay(500);  
+}
